edce3bf3f2ae2aedd53ae3277737eeb3da724a24
Ravi Kant <ravi.kant@magicsw.com>
2019-03-05 05:57:15 -0800

    save

--- src/App.js
+++ src/App.js
@@ -48,8 +48,10 @@ class App extends Component {
            }}/>
         ) }
          </Transition>
-        { this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
-        { this.state.modalIsOpen  ? <Backdrop closed={this.closeModal} />: null}
+  
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+
+        { this.state.modalIsOpen  ? <Backdrop show  />: null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
